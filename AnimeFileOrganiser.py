7562c7ad783da7c85ba3d17e5bc98b529d600390
Johnny Kaime <johnnyhou789@gmail.com>
2020-12-08 14:28:58 +0200

    Update AnimeFileOrganiser.py

--- AnimeFileOrganiser.py
+++ AnimeFileOrganiser.py
@@ -23,6 +23,13 @@
 #				skip
 
 import os, re, shutil
+import tkinter
+from tkinter import filedialog
+from tkinter import messagebox
+
+input_dir = ""
+sorted_dir = ""
+anime_dict = {}
 
 def cleanPrint():
 	for key, value in anime_dict.items():
@@ -30,51 +37,56 @@ def cleanPrint():
 		value = value.split("#")
 		print("\n".join(value))
 
-#from tkinter import filedialog
+def askForDir():
+	#might use filedialog for selection
+	temp_dir = ""
+	while not os.path.isabs(temp_dir):
+		temp_dir = filedialog.askdirectory(initialdir=os.getcwd(),title="Select folder to sort")
+	global input_dir
+	input_dir = temp_dir.replace("/","\\\\")
+	temp_dir = ""
+	while not os.path.isabs(temp_dir):
+		temp_dir = filedialog.askdirectory(initialdir=os.getcwd(),title="Select folder to be sorted")
+		global sorted_dir
+	sorted_dir = temp_dir.replace("/","\\\\")
 
-#might use filedialog for selection
-#directory = filedialog.askdirectory(initialdir=os.getcwd(),title="Select folder to sort")
+def scan():
+	for root, dirs, files in os.walk(os.path.abspath(input_dir)):
 
-input_dir = "E:\\Test"
-sorted_dir = "E:\\Test"
-anime_dict = {}
+		for file in files:
+			if file.endswith(('.mkv','.mp4')):
+				#Conditions
+				anime_name = file
+				clean_anime_name = ""
+				if "horriblesubs" in file.lower():
+					clean_anime_name = re.sub("[\(\[].*?[\)\]]", "", anime_name)
+					clean_anime_name = clean_anime_name.split("-")[0].strip()
+				elif "jyfansub" in file.lower() or "lksub" in file.lower() or "ktxp" in file.lower():
+					clean_anime_name = re.findall("\[(.*?)\]",anime_name)[1]
+				elif "erai-raws" in file.lower():
+					clean_anime_name = re.findall('\](.*?)\[',anime_name);
+					lastOccurOfHyphen = clean_anime_name[0].rindex("-");
+					clean_anime_name = clean_anime_name[0][:lastOccurOfHyphen].strip()
 
-while not os.path.isabs(input_dir):
-	input_dir = input("Enter folder location to be sort: C:/Users/JohnnyKaime/ \n")
+				if clean_anime_name not in anime_dict:
+					anime_dict[clean_anime_name] = os.path.join(root, file)
+				else:
+					anime_dict[clean_anime_name] = anime_dict[clean_anime_name]+"#"+os.path.join(root, file)
+		#Dont want to go into sub folders
+		#Uncomment if you want
+		break
+	#print(list_directories)
 
-while not os.path.isabs(sorted_dir):
-	sorted_dir = input("Enter folder location for files and folders to be moved: C:/Users/JohnnyKaime/Sorted \n")
+def move():
+	for key, value in anime_dict.items():
+		if not os.path.exists(os.path.join(sorted_dir,key)):
+			os.makedirs(os.path.join(sorted_dir,key))
 
-for root, dirs, files in os.walk(os.path.abspath(input_dir)):
+		anime_list = value.split("#")
+		for episode in anime_list:
+			shutil.move(episode,os.path.join(sorted_dir,key))
 
-	for file in files:
-		if file.endswith(('.mkv','.mp4')):
-			#Conditions
-			anime_name = file
-			clean_anime_name = ""
-			if "horriblesubs" in file.lower():
-				clean_anime_name = re.sub("[\(\[].*?[\)\]]", "", anime_name)
-				clean_anime_name = clean_anime_name.split("-")[0].strip()
-			elif "jyfansub" in file.lower() or "lksub" in file.lower() or "ktxp" in file.lower():
-				clean_anime_name = re.findall("\[(.*?)\]",anime_name)[1]
-			elif "erai-raws" in file.lower():
-				clean_anime_name = re.findall('\](.*?)\[',anime_name);
-				lastOccurOfHyphen = clean_anime_name[0].rindex("-");
-				clean_anime_name = clean_anime_name[0][:lastOccurOfHyphen].strip()
-
-			if clean_anime_name not in anime_dict:
-				anime_dict[clean_anime_name] = os.path.join(root, file)
-			else:
-				anime_dict[clean_anime_name] = anime_dict[clean_anime_name]+"#"+os.path.join(root, file)
-	#Dont want to go into sub folders
-	#Uncomment if you want
-	break
-#print(list_directories)
-
-for key, value in anime_dict.items():
-	if not os.path.exists(os.path.join(sorted_dir,key)):
-		os.makedirs(os.path.join(sorted_dir,key))
-
-	anime_list = value.split("#")
-	for episode in anime_list:
-		shutil.move(episode,os.path.join(sorted_dir,key))
+askForDir()
+scan()
+move()
+messagebox.showinfo("Completed", "All files are sorted from dir: {}\nto\ndir: {}".format(input_dir,sorted_dir))
